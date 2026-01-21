9a113669aeb8b8643d64ca6122b3f104a5d0d5ed
Heorhii Ponomarenko <ge.ponomarenko@gmail.com>
2026-01-21 22:45:04 +0100

    Added basic interface for user interaction with db and added requirements.txt

--- manager/models.py
+++ manager/models.py
@@ -5,8 +5,14 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     name = models.CharField(max_length=250)
-    description = models.CharField(max_length=400)
+    description = models.TextField()
     status = models.CharField(max_length=25)
     task_owner = models.ManyToManyField("User", related_name="owner")
+
+    def __str__(self):
+        return self.name
